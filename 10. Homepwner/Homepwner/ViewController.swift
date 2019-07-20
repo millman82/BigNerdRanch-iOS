35243112c5f6c64c71f4657a252feb3093949327
Tim Miller <millman@icloud.com>
2019-07-19 23:03:47 -0400

    Create new Homepwner project

---
+++ 10. Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Tim Miller on 7/19/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
