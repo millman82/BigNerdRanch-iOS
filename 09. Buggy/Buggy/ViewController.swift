67ee657c084d274e11df60398ebb2d3ebe2436e1
Tim Miller <millman@icloud.com>
2019-07-19 22:58:24 -0400

    Complete chapter 9

---
+++ 09. Buggy/Buggy/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Buggy
+//
+//  Created by Tim Miller on 7/18/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
+
+}
+
