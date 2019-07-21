e446d6b88638c7f48ea633d28f2f5ede10437ff3
Tim Miller <millman@icloud.com>
2019-07-20 23:16:48 -0400

    Display no more items as last row

--- 10. Homepwner/Homepwner/ItemsViewController.swift
+++ 10. Homepwner/Homepwner/ItemsViewController.swift
@@ -18,7 +18,7 @@ class ItemsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemStore.allItems.count + 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -26,13 +26,18 @@ class ItemsViewController: UITableViewController {
         // Get a new or recycled cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
-        // Set the text on the cell with the description of the item
-        // that is at the nth index of items, where n = row this cell
-        // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        if indexPath.row < itemStore.allItems.count {
+            // Set the text on the cell with the description of the item
+            // that is at the nth index of items, where n = row this cell
+            // will appear in on the tableview
+            let item = itemStore.allItems[indexPath.row]
+            
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        } else {
+            cell.textLabel?.text = "No more items!"
+            cell.detailTextLabel?.text = ""
+        }
         
         return cell
     }
