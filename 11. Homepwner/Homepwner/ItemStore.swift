e87b5b06ee9780bef320e0ed644b20587554fab7
Tim Miller <millman@icloud.com>
2019-07-21 19:37:07 -0400

    Add editing capabilities to table view

--- 11. Homepwner/Homepwner/ItemStore.swift
+++ 11. Homepwner/Homepwner/ItemStore.swift
@@ -20,9 +20,24 @@ class ItemStore {
         return newItem
     }
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
         }
     }
 }
