9267260b926dd42ef4f3628d156cc84b094b5178
Tim Miller <millman@icloud.com>
2019-07-21 23:53:42 -0400

    Set value label text color based on amount

--- 12. Homepwner/Homepwner/ItemCell.swift
+++ 12. Homepwner/Homepwner/ItemCell.swift
@@ -14,11 +14,33 @@ class ItemCell: UITableViewCell {
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateValueLabelTextColor()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         nameLabel.adjustsFontForContentSizeCategory = true
         serialNumberLabel.adjustsFontForContentSizeCategory = true
         valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    func updateValueLabelTextColor() {
+        
+        if let text = valueLabel.text {
+            
+            let index = text.index(text.startIndex, offsetBy: 1)
+            if let value = Double(text[index...]) {
+                
+                if value < 50 {
+                    valueLabel.textColor = UIColor.green
+                } else {
+                    valueLabel.textColor = UIColor.red
+                }
+            }
+        }
     }
 }
