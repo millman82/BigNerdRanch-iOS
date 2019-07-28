98fa955508a1d1f73ee8d8ca04fed58420abd44e
Tim Miller <millman@icloud.com>
2019-07-28 00:25:46 -0400

    Initial setup chapter 18

---
+++ 18. TouchTracker/TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Tim Miller on 7/28/19.
+//  Copyright © 2019 Tim Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
