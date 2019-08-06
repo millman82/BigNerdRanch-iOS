a74a87464417ba663bd9eaade542fb1d589b141c
Tim Miller <millman@icloud.com>
2019-08-06 15:37:15 -0400

    Build initial view controller

---
+++ 20. Photorama/Photorama/ViewControllers/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Tim Miller on 8/6/19.
+//  Copyright © 2019 Tim Miller. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
